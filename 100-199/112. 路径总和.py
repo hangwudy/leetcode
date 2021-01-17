6742936c78d4a8d3c72c5f9680a74e505fa8087f
Hang WU <hang.wu@yahoo.com>
2021-01-17 17:55:49 +0800

    广度优先搜索

---
+++ 100-199/112. 路径总和.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        dq_tree = deque([root])
+        dq_val = deque([root.val])
+        while dq_tree:
+            node = dq_tree.popleft()
+            tmp = dq_val.popleft()
+            if not node.left and not node.right:
+                if tmp == sum:
+                    return True
+                continue
+            if node.left:
+                dq_tree.append(node.left)
+                dq_val.append(tmp + node.left.val)
+            if node.right:
+                dq_tree.append(node.right)
+                dq_val.append(tmp + node.right.val)
+        return False
